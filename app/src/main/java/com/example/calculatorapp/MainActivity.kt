cade46e55793605e45d2469b2023053e495a5a5e
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-12 13:53:15 -1000

    created clear button

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+    val tvInput = findViewById<TextView>(R.id.tvInput)
+    val btnClear = findViewById<Button>(R.id.btnClear)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,7 +21,13 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit (view: View){
 
-        Toast.makeText(this, "Button Works", Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
+
     }
 
+    fun onClear (view: View){
+        tvInput.text = ""
+
+
+    }
 }
