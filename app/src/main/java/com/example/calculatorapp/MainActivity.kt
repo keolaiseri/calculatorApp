07450d6ab1629929057a886e24d42b9ae20d81f4
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-07-12 14:07:26 -1000

    working on decimal button

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     val tvInput = findViewById<TextView>(R.id.tvInput)
     val btnClear = findViewById<Button>(R.id.btnClear)
+    var lastNumeric  : Boolean = false
+    var lastDot : Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +24,25 @@ class MainActivity : AppCompatActivity() {
     fun onDigit (view: View){
 
         tvInput.append((view as Button).text)
-
+        lastNumeric = true
     }
 
     fun onClear (view: View){
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
 
+    fun onDecimalPoint (view: View){
+
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
 
     }
+
+
 }
+
